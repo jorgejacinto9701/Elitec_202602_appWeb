2fb0ce19a7dfb4af1873309ce6b41e10e9b29066
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2026-09-22 20:01:31 -0500

    inicio

---
+++ src/main/java/entity/Libro.java
@@ -0,0 +1,17 @@
+package entity;
+
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Libro {
+	private int idLibro;
+	private String registro;
+	private String titulo;
+	private String pais;
+	private String autor;
+	private LocalDate fechaCreacion;
+}
